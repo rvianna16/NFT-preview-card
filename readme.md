395a618bf9220172ecfd5fbe9172f0aea42782b7
Rodrigo Vianna <roodrigo.vs123@hotmail.com>
2022-01-09 20:28:31 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+<h1 align="center">
+    NFT preview card
+</h1>
+
+## 
+
+Desafio do Front-end mentor para prática do Tailwindcss
+
+---
+
+## :arrow_down: Preview
+
+<p align="center" >
+  <img alt="Web Homepage" title"Web-Home" src="https://i.imgur.com/YJd46s3.png" width="800px">  
+  <img alt="Web Homepage" title"Web-Home" src="https://i.imgur.com/qkXgY1M.png" width="800px">   
+  <br><br>    
+</p>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'soft-blue': 'hsl(215, 51%, 70%)',
+      'cyan': 'hsl(178, 100%, 50%)',
+      'body': 'hsl(217, 54%, 11%)',
+      'dark-blue': 'hsl(216, 50%, 16%)',
+      'blue': 'hsl(215, 32%, 27%)',
+      'white': 'hsl(0, 0%, 100%)'      
+    },
+    fontFamily: {
+      'sans': ['Outfit', 'sans-serif']
+    },
+    extend: { },
+  },
+  plugins: [],
+}
